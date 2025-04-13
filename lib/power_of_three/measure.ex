2b854e68e5172a24081bd38c1242fa4df7b20cbc
Igor Ostaptchenko <iostaptc@estee.com>
2025-04-13 12:08:58 -0400

    what

---
+++ lib/power_of_three/measure.ex
@@ -0,0 +1,18 @@
+defmodule PowerOfThree.Measure do
+  @moduledoc
+  """
+  https://cube.dev/docs/reference/data-model/measures
+  A Dimension of Cube object with following:
+  - name
+  - description
+  - drill_members
+  - filters
+  - format
+  - meta
+  - rolling_window
+  - public
+  - sql
+  - title
+  - type
+  """
+end
