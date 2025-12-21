445011e2a945fb83def74563320608e7a2d9ed69
Egor O'Sten <borodark@gmail.com>
2025-12-21 16:03:07 -0500

    CSV always wins

--- lib/power_of_three/cube_http_client.ex
+++ lib/power_of_three/cube_http_client.ex
@@ -279,43 +279,13 @@ defmodule PowerOfThree.CubeHttpClient do
   # Transforms row-oriented data to columnar format with type conversion
   defp transform_to_columnar([], _annotation), do: {:ok, %{}}
 
-  defp transform_to_columnar(rows, annotation) do
-    # annotation |> IO.inspect(label: :not_roze)
-    field_names = rows |> List.first() |> Map.keys()
-
-    # Transform each field to a column
-    dtypes =
-      field_names
-      |> Enum.map(fn field_name ->
-        {field_name, get_field_type(field_name, annotation)}
-      end)
-      |> Enum.into(%{})
-      |> Map.to_list()
-
-    dtypes |> IO.inspect(label: :dtypesZ)
-
-    """
-    TODO rename for casts
-    remanes =
-      for(
-        {col, _type} <- dtypes,
-        col |> String.ends_with?("count"),
-        do: {col, col |> String.replace(".", "_") |> String.to_atom()}
-      )
-      |> Enum.into(%{})
-
-    remanes |> IO.inspect(label: :remanes)
-    """
-
-    require Explorer.DataFrame
-
+  defp transform_to_columnar(rows, _annotations) do
     {
       :ok,
       Explorer.DataFrame.new(rows)
-      # TODO |> Explorer.DataFrame.rename("power_customers.count": :power_customers_count)
-      # TODO |> Explorer.DataFrame.mutate(power_customers_count: cast(power_customers_count, {:u, 64}))
+      |> Explorer.DataFrame.dump_csv!(quote_style: :non_numeric)
+      |> Explorer.DataFrame.load_csv!()
     }
-    |> IO.inspect(label: :DATAFRAME)
   rescue
     error ->
       {:error, QueryError.parse_error("Failed to transform response", error)}
